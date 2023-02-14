0487bc73547c8a95e69f86fe4da3270d457bfa18
Steve Brasier <steveb@stackhpc.com>
2023-02-14 14:11:08 +0000

    update submit.sh for location

--- submit.sh
+++ submit.sh
@@ -10,6 +10,6 @@ echo SLURM_JOB_ID: $SLURM_JOB_ID
 echo UCX_NET_DEVICES: $UCX_NET_DEVICES
 module load gnu12 openmpi4
 
-singularity exec io500.sif /io500/setup-run.sh ../config-debug-run.ini
+singularity exec io500.sif /io500/setup-run.sh ./config-debug-run.ini
 timestamp=$(date +%Y.%m.%d-%H.%M.%S)
-mpirun -np 2 singularity exec io500.sif /io500/io500 ../config-debug-run.ini --timestamp $timestamp
+mpirun -np 2 singularity exec io500.sif /io500/io500 ./config-debug-run.ini --timestamp $timestamp
