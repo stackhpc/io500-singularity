d344388b335f755dde6b66802e8d9d2cabb5c8d4
Steve Brasier <steveb@stackhpc.com>
2023-02-14 14:52:31 +0000

    parameterise singularity binary

--- submit.sh
+++ submit.sh
@@ -5,6 +5,7 @@
 #SBATCH --partition=compute
 
 IO500_TAG=0487bc7
+SINGULARITY=singularity
 
 export UCX_NET_DEVICES=all
 echo SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST
@@ -12,6 +13,6 @@ echo SLURM_JOB_ID: $SLURM_JOB_ID
 echo UCX_NET_DEVICES: $UCX_NET_DEVICES
 module load gnu12 openmpi4
 
-singularity exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_TAG} /io500/setup-run.sh ./config-debug-run.ini
+${SINGULARITY} exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_TAG} /io500/setup-run.sh ./config-debug-run.ini
 timestamp=$(date +%Y.%m.%d-%H.%M.%S)
-mpirun -np 2 singularity exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_TAG} /io500/io500 ./config-debug-run.ini --timestamp $timestamp
+mpirun -np 2 ${SINGULARITY} exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_TAG} /io500/io500 ./config-debug-run.ini --timestamp $timestamp
