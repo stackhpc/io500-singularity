011ea389f7929bcfb3b2d4fecdc77df8397d1809
Steve Brasier <steveb@stackhpc.com>
2023-02-14 14:37:57 +0000

    use ghcr build for run

--- submit.sh
+++ submit.sh
@@ -4,12 +4,14 @@
 #SBATCH --exclusive
 #SBATCH --partition=compute
 
+IO500_TAG=0487bc7
+
 export UCX_NET_DEVICES=all
 echo SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST
 echo SLURM_JOB_ID: $SLURM_JOB_ID
 echo UCX_NET_DEVICES: $UCX_NET_DEVICES
 module load gnu12 openmpi4
 
-singularity exec io500.sif /io500/setup-run.sh ./config-debug-run.ini
+singularity exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_TAG} /io500/setup-run.sh ./config-debug-run.ini
 timestamp=$(date +%Y.%m.%d-%H.%M.%S)
-mpirun -np 2 singularity exec io500.sif /io500/io500 ./config-debug-run.ini --timestamp $timestamp
+mpirun -np 2 singularity exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_TAG} /io500/io500 ./config-debug-run.ini --timestamp $timestamp
