59bb8613bf6fdf0bae38cc37d526abada2cae6d5
Steve Brasier <steveb@stackhpc.com>
2023-02-22 15:47:02 +0000

    use io500 binary directly in sbatch

--- submit.sh
+++ submit.sh
@@ -7,11 +7,11 @@
 
 export UCX_NET_DEVICES=all
 export IO500_CONTAINER_TAG="bb942db"
-export io500_cmd="singularity exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_CONTAINER_TAG} /io500"
 
 echo SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST
 echo SLURM_JOB_ID: $SLURM_JOB_ID
 echo UCX_NET_DEVICES: $UCX_NET_DEVICES
 module load gnu12 openmpi4
 
-./io500.sh $1
+timestamp=$(date +%Y.%m.%d-%H.%M.%S)
+mpirun singularity exec docker://ghcr.io/stackhpc/io500-singularity:${IO500_CONTAINER_TAG} /io500 $1 --timestamp $timestamp
